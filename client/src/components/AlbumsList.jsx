d50ba8880fea3c44f13a8a106bb6d9c575d9308e
jastr945 <polina.jastr@gmail.com>
2018-05-19 21:38:26 -0700

    Added delete button

--- client/src/components/AlbumsList.jsx
+++ client/src/components/AlbumsList.jsx
@@ -43,6 +43,7 @@ class AlbumsList extends React.Component {
                     <h2>{album.title}</h2>
                     <h6>{album.images.length} files - <Timestamp time={album.created_at} format='full' /> - <i><Timestamp time={album.created_at} format='ago' includeDay={true} precision={2} autoUpdate={60} /></i></h6>
                     <h5>{album.description}</h5>
+                    <h6 className="delete"><u>delete</u></h6>
                   </div>
                   <ImageRow
                   albums={this.props.albums}
