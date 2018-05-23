83e6c47c9b178218838c896d137033252068572c
jastr945 <polina.jastr@gmail.com>
2018-05-22 22:32:36 -0700

    Added 3 sample albums

--- manage.py
+++ manage.py
@@ -28,19 +28,41 @@ def test():
 @manager.command
 def seed_db():
     """Seeds the database."""
-    album1 = Album(title='polina\'s album', description="Polina's album with many awesome pictures.", user_email="example@example.com")
-    img1 = Image(name='pigs3.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/880e033191ca56e1612049fcf76565b7a8377911/subscriber/coffee5.jpg')
-    img2 = Image(name='pigs4.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/880e033191ca56e1612049fcf76565b7a8377911/subscriber/coffee2.jpg')
+    album1 = Album(title='Columbia River Gorge', description='The Columbia River Gorge is a canyon of the Columbia River in the Pacific Northwest of the United States.', user_email='example@example.com')
+    img1 = Image(name='1.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Columbia_River_Gorge/DSC03751.JPG')
+    img2 = Image(name='2.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Columbia_River_Gorge/DSC03754.JPG')
+    img3 = Image(name='3.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Columbia_River_Gorge/DSC03755.JPG')
+    img4 = Image(name='4.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Columbia_River_Gorge/DSC03756.JPG')
 
-    album2 = Album(title='pofi', description="Pofi's cute pictures.", user_email="example@example.com")
-    img3 = Image(name='pigs7.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/880e033191ca56e1612049fcf76565b7a8377911/subscriber/coffee.jpg')
-    img4 = Image(name='pigs8.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/880e033191ca56e1612049fcf76565b7a8377911/subscriber/coffee4.jpg')
+    album2 = Album(title='Crystal Springs Rhododendron Garden', description='Beautiful rhododendron garden in gorgeous Southeast Portland!', user_email="example@example.com")
+    img5 = Image(name='5.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03714.JPG')
+    img6 = Image(name='6.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03715.JPG')
+    img7 = Image(name='7.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03719.JPG')
+    img8 = Image(name='8.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03723.JPG')
+    img9 = Image(name='9.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03726.JPG')
+    img10 = Image(name='10.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03727.JPG')
+    img11 = Image(name='11.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03735.JPG')
+    img12 = Image(name='12.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03741.JPG')
+    img13 = Image(name='13.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03744.JPG')
+    img14 = Image(name='14.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03745.JPG')
+    img15 = Image(name='15.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Crystal_Springs_Rhododendron_Garden/DSC03746.JPG')
 
-    album1.images = [img1, img2]
-    album2.images = [img3, img4]
+    album3 = Album(title='Magnolia Trail in the Washington Park', description='Hoyt Arboretum, Portland, Oregon. Spring 2018.', user_email='example@example.com')
+    img16 = Image(name='16.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03639.JPG')
+    img17 = Image(name='17.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03642.JPG')
+    img18 = Image(name='18.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03643.JPG')
+    img19 = Image(name='19.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03644.JPG')
+    img20 = Image(name='20.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03655.JPG')
+    img21 = Image(name='21.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03654.JPG')
+    img22 = Image(name='22.jpg', url='https://raw.githubusercontent.com/jastr945/xperiments/master/flask-photoalbum/sample_albums/Magnolia_Trail_in_the_Washington_Park/DSC03660.JPG')
+
+    album1.images = [img1, img2, img3, img4]
+    album2.images = [img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15]
+    album3.images = [img16, img17, img18, img19, img20, img21, img22, img13]
 
     db.session.add(album1)
     db.session.add(album2)
+    db.session.add(album3)
     db.session.commit()
 
 
